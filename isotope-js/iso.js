e21e753fb047d17ffe5cd954679b38c5f0adda49
MitchBrenner <mkbrenner3@wisc.edu>
2022-05-26 20:22:19 -0500

    Updated grid-items
    Also added data structure button and animation

--- isotope-js/iso.js
+++ isotope-js/iso.js
@@ -2,7 +2,8 @@ const allBtn = document.getElementById("all")
 const javaBtn = document.getElementById("java")
 const jsBtn = document.getElementById("javascript")
 const pythonBtn = document.getElementById("python")
-
+const dataBtn = document.getElementById("data")
+const data = document.querySelectorAll(".data")
 
 var grid = $('.grid').isotope({
     // options
@@ -15,22 +16,50 @@ var grid = $('.grid').isotope({
     grid.isotope({
         filter: '*'
     })
+    removeDataSytling()
+
   })
 
   javaBtn.addEventListener('click', () => {
     grid.isotope({
         filter: '.java'
     })
+    removeDataSytling()
+
   })
 
   jsBtn.addEventListener('click', () => {
     grid.isotope({
         filter: '.javascript'
     })
+    removeDataSytling()
+
   })
 
   pythonBtn.addEventListener('click', () => {
     grid.isotope({
         filter: '.python'
     })
+    removeDataSytling()
+
   })
+
+  dataBtn.addEventListener('click', () => {
+    grid.isotope({
+        filter: '.data'
+    })
+
+    addDataStyling()
+  })
+
+  
+  function addDataStyling(){
+    data.forEach(element => {
+      element.classList.add("data-styling")
+    });
+  }
+  function removeDataSytling(){
+    data.forEach(element => {
+      element.classList.remove("data-styling")
+    });
+  }
